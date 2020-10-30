4b054e95a39185b144940bf7fc858a2e87d8eb26
Jørn Lode <jlode90@gmail.com>
2020-10-31 00:23:34 +0100

    🤷

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -15,14 +15,56 @@
   services = {
     redshift = {
       enable = true;
-      provider = "geoclue2";
+      latitude = "60.38";
+      longitude = "5.32";
       temperature = {
         day = 3500;
         night = 3000;
       };
     };
 
-    dunst.enable = true;
+    dunst = {
+      enable = true;
+      settings = {
+        global = {
+          font = "Monospace 10";
+          allow_markup = true;
+          format = "<b>%s</b>\\n%b";
+          geometry = "300x10-0-0";
+          sticky_history = true;
+          history_length = 50;
+          padding = 8;
+          horizontal_padding = 8;
+        };
+        frame = {
+          width = 3;
+          color = "#aaaaaa";
+        };
+        shortcuts = {
+          close = "ctrl+grave";
+          history = "mod1+grave";
+        };
+        urgency_low = {
+          background = "#222222";
+          foreground = "#888888";
+          timeout = 10;
+        };
+        urgency_normal = {
+          background = "#285577";
+          foreground = "#ffffff";
+          timeout = 20;
+        };
+        urgency_critical = {
+          background = "#900000";
+          foreground = "#ffffff";
+          timeout = 0;
+        };
+        ignore = {
+          appname = "Spotify";
+          format = "";
+        };
+      };
+    };
     unclutter.enable = true;
   };
 
--- system/boot.nix
+++ system/boot.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 let
   cfg = import ../settings.nix;
 in
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages;
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = lib.mkIf cfg.oldIntel [ "intel_pstate=active" ];
     tmpOnTmpfs = ! cfg.conserveMemory;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
