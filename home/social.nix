f2e2f11b1fcf7c88dd754e37c6ecf24af479fb53
Jørn Lode <jld@datarepons.no>
2023-11-24 08:46:41 +0100

    Setup for videoconferencing with teams+obs

--- home/social.nix
+++ home/social.nix
@@ -1,19 +1,9 @@
 { pkgs, lib, ... }:
 
-let
-  cfg = import ../settings.nix;
-  discord = pkgs.discord.override rec {
-    version = "0.0.26";
-    src = builtins.fetchurl {
-      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "MPdNxZJBmIN4NGEoYWvL2cmNm37/YT275m2bVWHXbwY=";
-    };
-    nss = pkgs.nss_latest;
-  };
-in
-  {
-    home.packages = with pkgs; [
-      discord
-      # weechat
-    ];
-  }
+{
+  home.packages = with pkgs; [
+    # for video conferencing
+    obs-studio
+    chromium # teams doesn't work correctly in firefox
+  ];
+}
--- system/boot.nix
+++ system/boot.nix
@@ -1,10 +1,15 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = import ../settings.nix;
 in
 {
   boot = {
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=9 card_label="Virtual Camera"
+    '';
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = lib.mkIf cfg.oldIntel [ "intel_pstate=active" ];
     tmp.useTmpfs = ! cfg.conserveMemory;
